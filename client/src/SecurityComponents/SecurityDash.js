6f5c6ca8aa15a875b7e1aa49924c1693ad23ea91
hd442822 <90863154+hd442822@users.noreply.github.com>
2023-11-19 23:04:11 -0500

    Create SecurityDash.js

---
+++ client/src/SecurityComponents/SecurityDash.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SecurityNavbar from './SecurityNavbar'
+import GuestParkingManager from './GuestParkingManager'
+
+export default function SecurityDash() {
+  return (
+    <div>
+      <SecurityNavbar/>
+      <GuestParkingManager/>
+    </div>
+  )
+}
