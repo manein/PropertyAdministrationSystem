f39f662668abd94d16c89d6d9da40bc3f5d1e472
Jeevan Reddy Saddikuti <sjeevanreddy007@gmail.com>
2023-11-21 00:52:58 -0500

    app/index
    updated code

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,66 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import ProtectedElement from './Auth_files/ProtectedElement';
+import { AuthProvider } from  './Auth_files/AuthProvider';
+
+import Home from './Home';
+import Navbar from './NavBar';
+import GuestParking from './GuestParking';
+import AboutUs from './Aboutus';
+import Login from './Login';
+import SignUp from './SignUp';
+import ManageTenants from './AdminComponents/ManageTenants';
+import ManageAnnouncements from './AdminComponents/ManageAnnouncements';
+import ViewAnnouncements from './TenantComponets/ViewAnnouncements';
+import PostMaintenanceRequest from './TenantComponets/PostMaintenanceRequest';
+import MaintenanceRequestList from './MaintainceTeamComponents/MaintenanceRequestList';
+import TenantMaintenanceRequests from './TenantComponets/TenantMaintenanceRequests';
+import RequestGuestParking from './TenantComponets/RequestGuestParking';
+import GuestParkingManager from './SecurityComponents/GuestParkingManager';
+import TenantGuestParkingStatus from './TenantComponets/TenantGuestParkingStatus';
+
+import AdminDash from './AdminComponents/AdminDash';
+import MaintenanceDash from './MaintainceTeamComponents/MaintenanceDash';
+import TenantDash from './TenantComponets/TenantDash';
+import SecurityDash from './SecurityComponents/SecurityDash';
+    
 
 function App() {
-  
-    
+    return (
+        <AuthProvider>
+          <Router>
+            <Routes>
+            <Route path="/admin"  element={<ProtectedElement><AdminDash /></ProtectedElement> } />
+                <Route path="/maintenance" element={<ProtectedElement><MaintenanceDash /></ProtectedElement> } />
+                <Route path="/tenant"  element={<ProtectedElement><TenantDash /></ProtectedElement> } />
+                <Route path="/security"   element={<ProtectedElement><SecurityDash /></ProtectedElement> } />
+
+                <Route path="/home" element={<Home />}/>
+                <Route path="/navbar" element={<Navbar />}/>
+                <Route path="/guestparking" element={<GuestParking />}/>
+                <Route path="/aboutus" element={<AboutUs />}/>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/" element={<SignUp />}/>
+                <Route path="/signup" element={<SignUp />}/>
+                <Route path="/manageTenant" element={<ManageTenants />}/>
+                <Route path="/manageAnnounc" element={<ManageAnnouncements />}/>
+                <Route path="/viewAnnounc" element={<ViewAnnouncements />}/>
+                <Route path="/postMainReq" element={<ProtectedElement><PostMaintenanceRequest /></ProtectedElement>}/>
+                
+                <Route path="/MyMainReq" element={<ProtectedElement><TenantMaintenanceRequests /></ProtectedElement>}/>
+                
+                <Route path="/reqGP" element={<ProtectedElement><RequestGuestParking /></ProtectedElement>}/>
+                <Route path="/manageGP" element={<ProtectedElement><GuestParkingManager /></ProtectedElement>}/>
+                
+                <Route path="/viewMyGPStatus" element={<ProtectedElement><TenantGuestParkingStatus /></ProtectedElement>}/>
+                
+
+                <Route path="/MainReqList" element={<ProtectedElement><MaintenanceRequestList /></ProtectedElement>}/>
+            </Routes>
+      </Router>
+    </AuthProvider>
+      );
 
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,36 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import Home from './Home';
-import Navbar from './NavBar';
-import GuestParking from './GuestParking';
-import AboutUs from './Aboutus';
-import Login from './Login';
-
-const routes = createRoutesFromElements(
-    <>
-    <Route path="/home" element={<Home />}/>
-    <Route path="/navbar" element={<Navbar />}/>
-    <Route path="/guestparking" element={<GuestParking />}/>
-    <Route path="/aboutus" element={<AboutUs />}/>
-    <Route path="/login" element={<Login />}/>
-    </>
-  )
-
-  const router = createBrowserRouter(routes);
+// import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <RouterProvider router={router}>
     <App />
-    </RouterProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
